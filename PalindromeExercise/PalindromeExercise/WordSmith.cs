642205711f116f22da9572a3f3318fe88e048090
murra <murraymalik2@gmail.com>
2023-08-02 19:10:32 -0400

    wordsmith

---
+++ PalindromeExercise/PalindromeExercise/WordSmith.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+
+namespace PalindroneExercise
+{
+    public class WordSmith
+    {
+        public bool IsAPalindrone (string word)
+        {
+            //reverse the word // racecar Length -->7 --> 6
+            var reversed = "";
+        for (int i = word.Length -1 ; i >= 0; i--) 
+            { 
+            reversed += word[i];
+            }
+        //compared the original with the reversed
+        if (reversed.ToLower() == word.ToLower()) 
+            { 
+            return true;
+            }
+            else
+            { 
+            return false;
+            }
+         //return whether they are the same
+
+        
+        }
+    }
+}
